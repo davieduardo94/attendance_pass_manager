92dbca001c957d9775e886979b28bb1b954c7dff
Davi Eduardo Gama Barbosa <davieduardo94@gmail.com>
2022-09-19 23:39:46 -0400

    create class user

---
+++ user_attendance.py
@@ -0,0 +1,5 @@
+class UserAttendance():
+    def __init__(self, name, attendance, enterDate) -> None:
+        self.name = name
+        self.attendance = attendance
+        self.enterDate = enterDate
