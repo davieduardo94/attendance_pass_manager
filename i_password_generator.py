c04aafcf33fd3850b9e248ac0f302effd361536c
Davi Eduardo Gama Barbosa <davieduardo94@gmail.com>
2023-10-21 16:06:36 -0400

    class comment

--- i_password_generator.py
+++ i_password_generator.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractclassmethod
 
 # Interface
 class PasswordGenerator(ABC):
+    """
+    Obrigando que as classes tenham este metodo
+    """
     @abstractclassmethod
     def generatePwd(self):
         pass
