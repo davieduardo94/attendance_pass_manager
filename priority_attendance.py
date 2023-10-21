149ee519813c37d69881b1ead5bc2b9c5f24aa04
Davi Eduardo Gama Barbosa <davieduardo94@gmail.com>
2023-10-21 16:05:34 -0400

    interface PasswordManager and createPwd method

---
+++ priority_attendance.py
@@ -0,0 +1,13 @@
+from password_manager import PasswordManager
+from password_priority import PriorityPassword
+
+
+class PriorityAttendance(PasswordManager):
+    def __init__(self, attendance_type):
+        self.attendance_type = attendance_type
+        
+    def createPwd(self):
+        """
+        Criando a senha de acordo com o atendimento
+        """
+        return PriorityPassword(self.attendance_type)
