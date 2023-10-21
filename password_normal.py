323b766aaf6ae9f98020963e8e44921bf181bbb3
Davi Eduardo Gama Barbosa <davieduardo94@gmail.com>
2023-10-21 16:06:27 -0400

    method generatePwd and interface with PasswordGenerator

--- password_normal.py
+++ password_normal.py
@@ -1,7 +1,21 @@
-class NormalPassword:
-    __init = None
-    __password = None
-    __last_password = None
+from i_password_generator import PasswordGenerator
+
+# Implementando a interface PasswordGenerator
+class NormalPassword(PasswordGenerator):
+    __init = 0
+    __password = 0
+    __last_password = 0
     
+    def __init__(self, attendance_type):
+        self.attendance_type = attendance_type
+        
+
     def generatePwd(self):
-        pass
+        if self.__init == 0:
+            self.__password = self.__init + 1
+            self.__init += 1
+        else:
+            self.__password = self.__init + 1
+            self.__init += 1
+        result = "Senha &: \nN%".format(self.attendance_type,self.__password)
+        return result
--- password_priority.py
+++ password_priority.py
@@ -1,7 +1,21 @@
-class PriorityPassword:
-    __init = None
-    __password = None
+from i_password_generator import PasswordGenerator
+
+
+class PriorityPassword(PasswordGenerator):
+    __init = 0
+    __password = 0
     __last_password = None
     
+    def __init__(self, attendance_type):
+        self.attendance_type = attendance_type
+        
+
     def generatePwd(self):
-        pass
+        if self.__init == 0:
+            self.__password = self.__init + 1
+            self.__init += 1
+        else:
+            self.__password = self.__init + 1
+            self.__init += 1
+        result = "Senha &: \nP%".format(self.attendance_type,self.__password)
+        return result
